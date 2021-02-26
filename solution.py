054e79fbd3c1128afbc900bf3534d04f33674fbb
saibalacybersec <sl8062@nyu.edu>
2021-02-26 14:50:32 -0500

    Commit 2/26-8

--- solution.py
+++ solution.py
@@ -27,9 +27,43 @@ def smtp_client(port='1025', mailserver='127.0.0.1'):
          if recv1[:3] != '250':
                print('250 reply not received from server.')
                quit
+               # Send MAIL FROM command and print server response.
+
+         # MAIL FROM
+         mailFrom = "MAIL FROM:<sl8062@nyu.edu>\r\n"
+         clientSocket.send(mailFrom.encode())
+         recv2 = clientSocket.recv(1024)
+         recv2 = recv2.decode()
+         # print(recv2)
+
+        # Send RCPT TO command and print server response.
+         sendTo = 'RCPT TO:<sl8062@nyu.edu>\r\n'
+         clientSocket.send(sendTo.encode())
+         recv3 = clientSocket.recv(1024)
+         recv3 = recv3.decode()
+         # print(recv3)
+
+        # Send DATA command and print server response.
+         data = 'DATA\r\n'
+         clientSocket.send(data.encode())
+         recv4 = clientSocket.recv(1024)
+         recv4 = recv4.decode()
+         # print(recv4)
+
+        # Send message data.
+         clientSocket.send(msg.encode())
+         # Message ends with a single period.
+         clientSocket.send(endmsg.encode())
+         recv6 = clientSocket.recv(1024)
+         recv6 = recv6.decode()
+         # print(recv6)
+
+        # Send QUIT command and get server response.
+         quit = "QUIT\r\n"
+         clientSocket.send(quit.encode())
+         recv_7 = clientSocket.recv(1024)
+
          clientSocket.close()
-   # except:
-   #       clientSocket.close()
 
 if __name__ == '__main__':
    # smtp_client(25, 'smtp.nyu.edu')
